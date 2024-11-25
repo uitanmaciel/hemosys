2093f48ccb7e02e457d1578a8b4717447f8ea78d
uitanmaciel <uitan.s.maciel@gmail.com>
2024-11-25 05:38:51 -0300

    :sparkles: fear: Implemented messaging support

---
+++ contexts/shared-kernel/HemoSys.BrokerMessage/RabbitMqConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+
+namespace HemoSys.BrokerMessage;
+
+public class RabbitMqConfiguration
+{
+    public string Host { get; set; } = null!;
+    public int Port { get; set; }
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Exchange { get; set; } = null!;
+    public string ExchangeType { get; set; } = null!;
+    public string Retry { get; set; } = null!;
+    public IModel Channel { get; set; }
+
+    public RabbitMqConfiguration()
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = Host,
+            Port = Port,
+            UserName = Username,
+            Password = Password
+        };
+        var connection = factory.CreateConnection();
+        Channel = connection.CreateModel();
+        Channel.ExchangeDeclare(Exchange, ExchangeType, true);
+    }
+}
