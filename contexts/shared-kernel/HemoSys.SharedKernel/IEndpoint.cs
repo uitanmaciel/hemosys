d825d033c7978068b046945122b3a12b1fad7991
uitanmaciel <uitan.s.maciel@gmail.com>
2024-11-25 05:07:25 -0300

    :sparkles: feat: Shared Kernel has been implemented

---
+++ contexts/shared-kernel/HemoSys.SharedKernel/IEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace HemoSys.SharedKernel;
+
+/// <summary>
+/// Represents a contract for defining and mapping API endpoints within an application.
+/// </summary>
+public interface IEndpoint
+{
+    /// <summary>
+    /// Maps the endpoint(s) defined in the implementing class to the provided <see cref="IEndpointRouteBuilder"/>.
+    /// </summary>
+    /// <param name="app">The <see cref="IEndpointRouteBuilder"/> used to configure the application's endpoints.</param>
+    static abstract void Map(IEndpointRouteBuilder app);
+}
---
+++ contexts/shared-kernel/HemoSys.SharedKernel/State/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace HemoSys.SharedKernel.State;
+
+public interface IUnitOfWork : IDisposable, IAsyncDisposable
+{
+    Task BeginTransactionAsync(CancellationToken cancellationToken);
+    Task<bool> CommitAsync(CancellationToken cancellationToken);
+    Task RollbackAsync(CancellationToken cancellationToken);
+}
