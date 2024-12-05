73dbe7914f28b5b486c120e11c3097a457da1dd8
uitanmaciel <uitan.s.maciel@gmail.com>
2024-12-05 06:08:33 -0300

    :sparkles: feat: Implemented event handler for appointment update

---
+++ contexts/donation-management/ms-scheduling/core/HemoSys.SchedulingService.Application/Appointments/Events/Handlers/AppointmentUpdatedEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using HemoSys.SharedKernel.Stream;
+
+namespace HemoSys.SchedulingService.Application.Appointments.Events.Handlers;
+
+public class AppointmentUpdatedEventHandler(IProducer producer) : IRequestHandler<AppointmentUpdated>
+{
+    public async Task Handle(AppointmentUpdated request, CancellationToken cancellationToken)
+    {
+        var @event = request.ToMessage();
+        await producer.PublishAsync(
+            destination: @event.destination, 
+            eventName: @event.eventType, 
+            message: @event.message);
+    }
+}
