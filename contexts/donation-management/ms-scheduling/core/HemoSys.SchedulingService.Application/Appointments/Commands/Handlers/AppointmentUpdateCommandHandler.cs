2f9ae3b593cf35d2081fc44e954c6f10be2cd5cb
uitanmaciel <uitan.s.maciel@gmail.com>
2024-12-05 05:54:14 -0300

    :sparkles: feat: Implemented handler for appointment update

---
+++ contexts/donation-management/ms-scheduling/core/HemoSys.SchedulingService.Application/Appointments/Commands/Handlers/AppointmentUpdateCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace HemoSys.SchedulingService.Application.Appointments.Commands.Handlers;
+
+public class AppointmentUpdateCommandHandler(IAppointmentWriteRepository repository, ISender mediator) :
+    IRequestHandler<AppointmentUpdateCommand, bool>
+{
+    public async Task<bool> Handle(AppointmentUpdateCommand request, CancellationToken cancellationToken)
+    {
+        var appointment = request.ToDomain(request);
+        appointment.ApplyRulesToUpdateAppointment();
+        
+        var appointmentUpdate = await repository.UpdateAsync(appointment, cancellationToken);
+        if(!appointmentUpdate)
+        {
+            request.AddNotification("Appointment Update", "Error on update appointment.");
+            return false;
+        }
+        
+        var appointmentUpdated = new AppointmentUpdated(appointment);
+        await mediator.Send(appointmentUpdated, cancellationToken);
+        return true;
+    }
+}
---
+++ contexts/donation-management/ms-scheduling/core/HemoSys.SchedulingService.Application/Appointments/Events/Models/AppointmentUpdated.cs
@@ -0,0 +1,24 @@
+﻿namespace HemoSys.SchedulingService.Application.Appointments.Events.Models;
+
+public class AppointmentUpdated : IRequest
+{
+    private const string Destination = "schedulingevents";
+    private string EventType => string.Concat("appointment.", StatusTypes.ToString().ToLower());
+    private Guid Id { get; set; }
+    private string? Location { get; set; } 
+    private DateTime ScheduledDate { get; set; }
+    private AppointmentStatusTypes StatusTypes { get; set; }
+    
+    public AppointmentUpdated(Appointment appointment)
+    {
+        Id = appointment.Id;
+        Location = string.Concat(appointment.Location.Name,": ",appointment.Location.Address.ToString());
+        ScheduledDate = appointment.ScheduledDate;
+        StatusTypes = (AppointmentStatusTypes)appointment.StatusTypes;
+    }
+    
+    public (string destination, string eventType, object message) ToMessage()
+    {
+        return (Destination, EventType, this);
+    }
+}
