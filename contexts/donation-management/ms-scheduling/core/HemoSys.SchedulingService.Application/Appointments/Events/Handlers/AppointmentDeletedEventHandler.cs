057eb9fe413373fc442a4e0f61af8ef3a3a19100
uitanmaciel <uitan.s.maciel@gmail.com>
2024-12-16 05:33:39 -0300

    :sparkels: feat: Implemented handler for appointment deletion.

---
+++ contexts/donation-management/ms-scheduling/core/HemoSys.SchedulingService.Application/Appointments/Events/Handlers/AppointmentDeletedEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using HemoSys.SharedKernel.Stream;
+
+namespace HemoSys.SchedulingService.Application.Appointments.Events.Handlers;
+
+public class AppointmentDeletedEventHandler(IProducer producer) : IRequestHandler<AppointmentDeleted>
+{
+    public async Task Handle(AppointmentDeleted request, CancellationToken cancellationToken)
+    {
+        const string destination = "schedulingevents";
+        const string eventName = "AppointmentDeleted";
+        
+        await producer.PublishAsync(
+            destination: destination, 
+            eventName: eventName, 
+            message: request);
+    }
+}
