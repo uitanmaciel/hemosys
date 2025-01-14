ee599daf2958510c24f4b4d6d36c0cfc85a3bd84
uitanmaciel <uitan.s.maciel@gmail.com>
2025-01-14 12:49:22 -0300

    :sparkles: feat: Implemented Shared Kernel

---
+++ src/shared-kernel/HemoSys.SharedKernel/Stream/IConsumer.cs
@@ -0,0 +1,6 @@
+﻿namespace HemoSys.SharedKernel.Stream;
+
+public interface IConsumer
+{
+    Task SubscribeAsync<T>(string queue, string eventName, Func<T, Task> payload);
+}
---
+++ src/shared-kernel/HemoSys.SharedKernel/Stream/StreamConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace HemoSys.SharedKernel.Stream;
+
+public record StreamConfiguration
+{
+    public string Host { get; set; } = null!;
+    public int Port { get; set; }
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Exchange { get; set; } = null!;
+    public string ExchangeType { get; set; } = null!;
+    public string Retry { get; set; } = null!;
+}
